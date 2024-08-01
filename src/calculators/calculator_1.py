f91773423d1644238fa08e9ec118e94c46a6d572
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-08-01 22:29:40 +0000

    🆕 NEW: calculator_1

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -9,6 +9,10 @@ class Calculator1:
         splitted_number = input_data / 3
         
         first_process_result = self.__first_process(splitted_number)
+        second_process_result = self.__second_process(splitted_number)
+        calc_result = first_process_result + second_process_result + splitted_number
+        
+        return self.__format_response(calc_result)
 
     def __validate_body(self, body: Dict) -> float:
         if "number" not in body:
@@ -20,3 +24,16 @@ class Calculator1:
         first_part = first_number / 4 + 7
         second_part = first_part ** 2 * 0.257
         return second_part
+    
+    def __second_process(self, second_number: float) -> float:
+        first_part = second_number ** 2.121
+        second_part = first_part / 5 + 1
+        return second_part
+    
+    def __format_response(self, calc_result: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 1,
+                "result": round(calc_result, 2)
+            }
+        }
--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, jsonify, request
-from src.main.calculators.calculator_1 import Calculator1
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
     calculator1 = Calculator1()
-    calculator1.calculate(request)
+    response = calculator1.calculate(request)
     
-    return jsonify({
-        "success": True
-    }), 200
+    return jsonify(response), 200
