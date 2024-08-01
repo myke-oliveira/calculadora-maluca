ebdcc9fd66d90fd76ff537b313295d5de33465a9
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-08-01 22:20:47 +0000

    🆕 NEW: calculator_1

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, jsonify, request
+from src.main.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    print(request.json)
-    return jsonify({ "success": True }), 200
+    calculator1 = Calculator1()
+    calculator1.calculate(request)
+    
+    return jsonify({
+        "success": True
+    }), 200
