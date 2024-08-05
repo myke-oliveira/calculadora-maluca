cb720ed63f0c6bf1ce8de0e08356bb2d9bc64d23
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-08-05 21:34:35 +0000

    🆕 NEW: route /calculator/2

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from src.calculators.calculator_1 import Calculator1
+from src.calculators.calculator_2 import Calculator2
+from src.drivers.numpy_handler import NumpyHandler
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
@@ -7,5 +9,13 @@ calc_route_bp = Blueprint("calc_routes", __name__)
 def calculator_1():
     calculator1 = Calculator1()
     response = calculator1.calculate(request)
+    
+    return jsonify(response), 200
+
+@calc_route_bp.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    numpy_handler = NumpyHandler()
+    calculator2 = Calculator2(numpy_handler)
+    response = calculator2.calculate(request)
     
     return jsonify(response), 200
