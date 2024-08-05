f6b9b10dca987b0c9be40ba09a8044d800424231
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-08-05 21:46:08 +0000

    🔨 REFACTOR: factories

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,21 +1,19 @@
 from flask import Blueprint, jsonify, request
-from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2
-from src.drivers.numpy_handler import NumpyHandler
+from src.main.factories.calculator1_factory import calculator1_factory
+from src.main.factories.calculator2_factory import calculator2_factory
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    calculator1 = Calculator1()
+    calculator1 = calculator1_factory()
     response = calculator1.calculate(request)
     
     return jsonify(response), 200
 
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
-    numpy_handler = NumpyHandler()
-    calculator2 = Calculator2(numpy_handler)
+    calculator2 = calculator2_factory()
     response = calculator2.calculate(request)
     
     return jsonify(response), 200
